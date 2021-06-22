2a5358052f2fa13239c8da48b55915df1288f099
adam668 <eladdouliadam3@gmail.com>
2021-06-22 16:09:55 -0400

    fixed

--- tictactoe/Source.cpp
+++ tictactoe/Source.cpp
@@ -17,7 +17,7 @@ bool verifyWin(char* sides) {
 
 bool checkTaken(int objectToFind, int Array[9]) {	
 
-	for (int i = 0; i <= 9; i++)
+	for (int i = 0; i <= 8; i++)
 	{
 		if (Array[i] == objectToFind) {
 			return true;
